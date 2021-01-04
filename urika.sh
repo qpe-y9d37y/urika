e7e97ede43515c63c6c61064f173fdf61965ae5c
Quentin <44425947+qpe-y9d37y@users.noreply.github.com>
2021-01-04 13:37:07 +0100

    Update urika.sh

--- urika.sh
+++ urika.sh
@@ -9,7 +9,7 @@
 # Current version: 0.1                                                 #
 # Status: Work in progress                                             #
 #                                                                      #
-# This script purpose is to set up an entertainment system.            #
+# This script purpose is to setup an entertainment system.             #
 #                                                                      #
 # Version history:                                                     #
 # +----------+--------+------+---------------------------------------+ #
@@ -28,49 +28,14 @@
 #                               FUNCTIONS                              #
 #                                                                      #
 
-#usage () {
-#}
+# Function to print usage.
+function usage {
+  echo "usage: $(basename $0) [-h]
 
-wlan_setup () {
-  # Discover wireless interface
-  IF_WLAN=$(ip l | grep -v link | awk '{print $2}' | sed 's/.$//' | grep "^w")
+Setup an entertainment system.
 
-  # Confirm interface
-  echo "Set interface ${IF_WLAN}?"
-  select ANS_IFWLAN in "Yes" "No"; do
-    case ${ANS_IFWLAN} in
-      Yes ) break ;;
-      No )
-        # Choose interface
-	select ANS_IFCHOICE in $(ip l | grep -v link | awk '{print $2}' | sed 's/.$//' | grep -v 'lo'); do
-	  IF_WLAN=${ANS_IFCHOICE}
-	  break
-	done
-	break
-      ;;
-    esac
-  done
-
-  # Write wireless configuration
-#  cat > /etc/wpa_supplicant/wpa_supplicant.conf << EOM
-#network={
-#  proto=RSN
-#  key_mgmt=WPA-PSK
-#  group=CCMP
-#  pairwise=CCMP
-#  priority=10
-#  ssid="FRITZ!Box 7560 ZY"
-#  psk="00510058006391613756"
-#}
-#EOM
-#
-#  # Write wireless interface configuration
-#  cat >> /etc/network/interfaces << EOM
-#
-#auto ${IF_WLAN}
-#iface ${IF_WLAN} inet dhcp
-#wpa-conf /etc/wpa_supplicant/wpa_supplicant.conf
-#EOM
+arguments:
+ -h, --help  show this help message"
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -78,32 +43,72 @@ wlan_setup () {
 #                               BEGINNING                              #
 #                                                                      #
 
-# Check network connection
+# Print usage if requested.
+if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
+  usage
+  exit 0
+fi
+
+# Check that script is launched by root.
+if [[ $(whoami) != "root" ]]; then
+  echo "This script should be executed by root, good bye." && exit 1
+fi
+
+# Check network connection.
 ping -c 1 8.8.8.8 > /dev/null
 if [[ $? == 0 ]]; then
   echo "Network reachable"
 else
   echo "Network not reachable"
 
-  # Ask if cfg network with script
+  # Ask if cfg network with script.
   echo "Do you want to configure the network?"
   select ANS_NETCFG in "Yes" "No"; do
     case ${ANS_NETCFG} in
       No ) echo "Cannot continue without reachable network, good bye" && exit 0 ;;
-      Yes ) 
-	# Ask which kind of network
-        echo "Wireless or ethernet?"
-	select ANS_NETTYPE in "Wireless" "Ethernet"; do
-	  case ${ANS_NETTYPE} in
-	    Wireless )
-	      wlan_setup
-	      break
-	    ;;
-	    Ethernet ) echo "Sorry not available yet" && exit 0 ;;
-	  esac
-        done
-	break
+      Yes )
+        ./util/deb_netconf.sh
+	    break
       ;;
     esac
   done
 fi
+
+# Create urika user.
+useradd -m urika
+
+# Update the package list.
+apt-get update
+
+# Install required packages.
+apt-get install sudo xorg openbox lightdm firefox
+
+# Enable autologin.
+cat > /etc/lightdm/lightdm.conf << EOM
+[SeatDefaults]
+autologin-user=urika
+user-session=openbox
+EOM
+
+# Create openbox config directory.
+mkdir -p /home/urika/.config/openbox
+chown -R urika:urika /home/urika/.config
+chmod 755 /home/urika/.config /home/urika/.config/openbox
+
+# Create autostart script.
+cat > /home/urika/.config/openbox/autostart << EOM
+#!/bin/bash
+while true; do
+  firefox --kiosk /home/urika/html/index.html &
+done
+EOM
+
+# Create html directory
+mkdir -p /home/urika/html/{images,icons}
+chown -R urika:urika /home/urika/html
+chmod -R 755 /home/urika/html
+
+#                                                                      #
+#                                  END                                 #
+#                                                                      #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
