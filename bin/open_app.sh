50bdc75a36c5ebd641afb724e4bff976d7105629
Quentin <44425947+qpe-y9d37y@users.noreply.github.com>
2021-01-04 16:13:26 +0100

    Update open_app.sh

--- bin/open_app.sh
+++ bin/open_app.sh
@@ -3,5 +3,7 @@
 APP=${1#app://}
 case ${APP} in
   molotov ) /home/urika/bin/molotov ;;
+  power ) gnome-terminal -- bash -c "/home/urika/bin/power_mgmt.sh"
+  settings ) gnome-terminal -- bash -c "/home/urika/bin/settings.sh"
   * ) nohup "${APP}" &>/dev/null & ;;
 esac
