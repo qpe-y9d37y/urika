a05601fa9e39ab5ae7c2899507b70817579b2e9b
Quentin <44425947+qpe-y9d37y@users.noreply.github.com>
2021-01-05 08:47:53 +0100

    Use template

--- bin/open_app.sh
+++ bin/open_app.sh
@@ -1,9 +1,54 @@
 #!/bin/bash
- 
+
+########################################################################
+# MIT License                             Copyright 2021 Quentin Petit #
+# January 2021                             <qpe-y9d37y@protonmail.com> #
+#                                                                      #
+#                              open_app.sh                             #
+#                                                                      #
+# Current version: 0.1                                                 #
+# Status: Work in progress                                             #
+#                                                                      #
+# This script purpose it to manage the power of the urika server and   #
+# client.                                                              #
+#                                                                      #
+# Version history:                                                     #
+# +----------+--------+------+---------------------------------------+ #
+# |   Date   | Author | Vers | Comment                               | #
+# +==========+========+======+=======================================+ #
+# | 20210104 | QPE    | 0.1  | Starting development                  | #
+# +----------+--------+------+---------------------------------------+ #
+#                                                                      #
+########################################################################
+
+#                                                                      #
+#                               VARIABLES                              #
+#                                                                      #
+
+# App to launch.
 APP=${1#app://}
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                                                      #
+#                               FUNCTIONS                              #
+#                                                                      #
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                                                      #
+#                               BEGINNING                              #
+#                                                                      #
+
+# Launch app.
 case ${APP} in
-  molotov ) /home/urika/bin/molotov ;;
-  power ) gnome-terminal -- bash -c "/home/urika/bin/power_mgmt.sh"
-  settings ) gnome-terminal -- bash -c "/home/urika/bin/settings.sh"
+  molotov ) /home/urika/bin/molotov & ;;
+  power ) gnome-terminal -- bash -c "/home/urika/bin/power_mgmt.sh" ;;
+  settings ) gnome-terminal -- bash -c "/home/urika/bin/settings.sh" ;;
   * ) nohup "${APP}" &>/dev/null & ;;
 esac
+
+#                                                                      #
+#                                 END                                  #
+#                                                                      #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
