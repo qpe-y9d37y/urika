a1fc7ee4a3a083c9c5e16c563974b029c91208c2
Quentin <44425947+qpe-y9d37y@users.noreply.github.com>
2021-01-06 09:45:52 +0100

    Add setting to change screen resolution

--- bin/settings.sh
+++ bin/settings.sh
@@ -26,13 +26,61 @@
 
 # Files and directories.
 INPUT=/tmp/menu.sh.$$
+RES_LINK="/etc/X11/Xsession.d/45setoutput"
+RES_SCRIPT="/home/urika/bin/setoutput.sh"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #                                                                      #
 #                               FUNCTIONS                              #
 #                                                                      #
 
+# Function to manage screen resolution
+function scresolution {
+  # Dialog menu.
+  dialog --clear --title "Settings" \
+    --menu "Select your choice:" 15 50 1 \
+    1 "Reset screen resolution" \
+    2 "Change screen resolution" 2>"${INPUT}"
 
+  # Register choice.
+  ITEM=$(<"${INPUT}")
+
+  # Launch selected action.
+  case ${ITEM} in
+    1)
+      if [[ -L ${RES_LINK} ]]; then
+        ${RES_LINK}
+      else
+        xrandr --auto
+      fi
+    ;;
+    2)
+      # Choose resolution with xrandr -q
+      # If chosen resolution is default
+      if [[ ${SCR_RES} == "Default" ]]; then
+        # Remove symlink if it exists
+        if [[ -L ${RES_LINK} ]]; then
+          rm ${RES_LINK}
+        fi
+        # Apply default resolution
+        xrandr --auto
+      # If chosen resolution is not default
+      else
+        # Set RES variable in ${RES_SCRIPT}
+        sed -i "s/^RES.*$/RES=\"${SCR_RES}\"/" ${RES_SCRIPT}
+        # Create symbolic link ${RES_LINK} -> ${RES_SCRIPT}
+        if [[ ! -L ${RES_LINK} ]]; then
+          ln -s ${RES_SCRIPT} ${RES_LINK}
+        elif [[ $(ls -l ${RES_LINK} | awk 'NF>1{print $NF}') == "${RES_SCRIPT}" ]]; then
+          ln -sf ${RES_SCRIPT} ${RES_LINK}
+        fi
+        # Set selected resolution
+        ${RES_LINK}
+      fi
+    ;;
+  esac
+    
+}
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #                                                                      #
@@ -42,16 +90,16 @@ INPUT=/tmp/menu.sh.$$
 # Launch dialog menu.
 dialog --clear --title "Settings" \
   --menu "Select your choice:" 15 50 1 \
-  1 "Update Urika" \ 2>"${INPUT}"
-#  2 "Set screen resolution"
+  1 "Update Urika" \
+  2 "Screen resolution" 2>"${INPUT}"
 
 # Register choice.
 ITEM=$(<"${INPUT}")
 
 # Launch selected action.
 case ${ITEM} in
-  1) sudo apt update && sudo apt upgrade;;
-  2) ;;
+  1) sudo apt update && sudo apt upgrade ;;
+  2) scresolution ;;
 esac
 
 #                                                                      #
