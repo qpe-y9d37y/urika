73118d9f1c79877ac5434f94382eb2090336867a
Quentin <44425947+qpe-y9d37y@users.noreply.github.com>
2021-01-06 09:04:11 +0100

    Use of template

--- bin/setoutput.sh
+++ bin/setoutput.sh
@@ -1,8 +1,51 @@
 #!/bin/sh
 
+########################################################################
+# MIT License                             Copyright 2021 Quentin Petit #
+# January 2021                             <qpe-y9d37y@protonmail.com> #
+#                                                                      #
+#                             setoutput.sh                             #
+#                                                                      #
+# Current version: 0.1                                                 #
+# Status: Work in progress                                             #
+#                                                                      #
+# This script purpose it to set screen output
+#                                                                      #
+# Version history:                                                     #
+# +----------+--------+------+---------------------------------------+ #
+# |   Date   | Author | Vers | Comment                               | #
+# +==========+========+======+=======================================+ #
+# | 20210106 | QPE    | 0.1  | Starting development                  | #
+# +----------+--------+------+---------------------------------------+ #
+########################################################################
+
+#                                                                      #
+#                               VARIABLES                              #
+#                                                                      #
+
+# Screen resolution
+RES="1024x768"
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                                                      #
+#                               FUNCTIONS                              #
+#                                                                      #
+
+
+
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
+#                                                                      #
+#                               BEGINNING                              #
+#                                                                      #
+
 # Set screen resolution
 xrandr --auto
-xrandr -s 1024x768
+xrandr -s ${RES}
 
 # List possible resolutions
 #xrandr -q
+
+#                                                                      #
+#                                 END                                  #
+#                                                                      #
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
