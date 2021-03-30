409c20ffc57b83c8f3b56c8c0267c524c26ec44d
Quentin <44425947+qpe-y9d37y@users.noreply.github.com>
2021-03-30 14:18:01 +0200

    Update urika.sh

--- urika.sh
+++ urika.sh
@@ -16,7 +16,7 @@
 # |   Date   | Author | Vers | Comment                               | #
 # +==========+========+======+=======================================+ #
 # | 20210103 | QPE    | 0.1  | Starting development                  | #
-# | 20210330 | QPE    | 0.2  | Simplify script to permit update      | #
+# | 20210330 | QPE    | 0.2  | Simplified and better looking         | #
 # +----------+--------+------+---------------------------------------+ #
 ########################################################################
 
@@ -30,6 +30,8 @@ URIKA_GRP="urika"
 
 # Files and directories.
 DIR_HOME="/home/urika"
+DIR_LOG="${DIR_HOME}/log"
+FIL_LOG="${DIR_LOG}/$(basename ${0} | sed 's/.sh/.log/')"
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
 #                                                                      #
@@ -41,9 +43,8 @@ function usage {
   echo "usage: $(basename $0) [-h]
   
 Setup an entertainment system.
-
 arguments:
- -h, --help  show this help message"
+ -h, --help  show this help message" > /dev/tty
 }
 
 #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#
@@ -57,25 +58,67 @@ if [[ $1 == "-h" ]] || [[ $1 == "--help" ]]; then
   exit 0
 fi
 
+# Start logging.
+if [[ ! -d ${DIR_LOG} ]]; then
+  mkdir ${DIR_LOG}
+fi
+date > ${FIL_LOG}
+exec 2>> ${FIL_LOG} 1>> ${FIL_LOG}
+set -x
+
+# Print status.
+echo -n "Check prerequisites " > /dev/tty
+
 # Check that script is launched by root.
 if [[ $(whoami) != "root" ]]; then
-  echo "This script should be executed by root, good bye." && exit 1
+  echo -e "[\e[91mKO\e[0m]" > /dev/tty
+  echo -e "[\e[91mERR\e[0m] This script should be executed by root, good bye." > /dev/tty
+  exit 1
 fi
 
 # Check network connection.
 ping -c 1 8.8.8.8 > /dev/null
 if [[ $? != 0 ]]; then
-  echo "Network not reachable, good bye." && exit 1
+  echo -e "[\e[91mKO\e[0m]" > /dev/tty
+  echo -e "[\e[91mERR\e[0m] Network not reachable, good bye." > /dev/tty
+  exit 1
 fi
+
+# Check ${DIR_HOME}.
+if [[ ! -d ${DIR_HOME} ]]; then
+  echo -e "[\e[91mKO\e[0m]" > /dev/tty
+  echo -e "[\e[91mERR\e[0m] ${DIR_HOME} doesn't exist. Please read README.md." > /dev/tty
+  exit 1
+fi
+
+# Check ${URIKA_USR} exists.
+grep ${URIKA_USR} /etc/passwd
+if [[ $? != "0" ]]; then
+  echo -e "[\e[91mKO\e[0m]" > /dev/tty
+  echo -e "[\e[91mERR\e[0m] ${URIKA_USR} doesn't exist. Please read README.md." > /dev/tty
+  exit 1
+fi
+
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "Sudo rights         " > /dev/tty
 
 # Grant sudo rights to urika user.
 echo "${URIKA_USR} ALL = (ALL) NOPASSWD: ALL" > /etc/sudoers.d/00-Urika
+
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "Install packages    " > /dev/tty
 
 # Update the package list.
 apt-get update
 
 # Install required packages.
 apt-get install -y sudo xorg openbox lightdm firefox-esr gnome-terminal zenity curl pulseaudio alsamixergui
+
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "Lightdm config      " > /dev/tty
 
 # Create lightdm configuration directory.
 mkdir /etc/lightdm
@@ -86,6 +129,10 @@ cat > /etc/lightdm/lightdm.conf << EOM
 autologin-user=${URIKA_USR}
 user-session=openbox
 EOM
+
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "Openbox config      " > /dev/tty
 
 # Create openbox config directory.
 mkdir -p ${DIR_HOME}/.config/openbox
@@ -98,10 +145,22 @@ cat > ${DIR_HOME}/.config/openbox/autostart << EOM
 firefox --kiosk ${DIR_HOME}/html/index.html &
 EOM
 
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "File permissions    " > /dev/tty
+
 # Set correct permissions for html directory.
 chown -R ${URIKA_USR}:${URIKA_GRP} ${DIR_HOME}/html
 chmod 755 ${DIR_HOME}/html
 chmod 644 ${DIR_HOME}/html/index.html
+
+# Set correct permissions for bin directory.
+chown -R ${URIKA_USR}:${URIKA_GRP} ${DIR_HOME}/bin
+chmod -R 755 ${DIR_HOME}/bin
+
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "App launcher        " > /dev/tty
 
 # Create .desktop file for application launcher.
 cat > /usr/share/applications/appurl.desktop << EOM
@@ -117,9 +176,9 @@ EOM
 # Refresh mime types database.
 update-desktop-database
 
-# Set correct permissions for bin directory.
-chown -R ${URIKA_USR}:${URIKA_GRP} ${DIR_HOME}/bin
-chmod -R 755 ${DIR_HOME}/bin
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "Install Spotify     " > /dev/tty
 
 # Add spotify GPG key.
 curl -sS https://download.spotify.com/debian/pubkey_0D811D58.gpg | apt-key add - 
@@ -133,6 +192,10 @@ apt-get update
 # Install spotify.
 apt-get install -y spotify-client
 
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
+echo -n "Firefox config      " > /dev/tty
+
 # Set Firefox homepage.
 ### user_pref("browser.startup.homepage", "${DIR_HOME}/html/index.html");
 ### ~/.mozilla/firefox/PROFILE_NAME.default/user.js
@@ -142,6 +205,9 @@ if [[ -d ${DIR_HOME}/.mozilla ]]; then
     echo "user_pref(\"browser.startup.homepage\", \"${DIR_HOME}/html/index.html\");" > user.js && \
     chown ${URIKA_USR}:${URIKA_GRP} user.js
 fi
+
+# Print status.
+echo -e "[\e[92mOK\e[0m]" > /dev/tty
 
 #                                                                      #
 #                                  END                                 #
